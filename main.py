0e88bb2264cd567900a8ee1807eb01a5e1247084
Erin O'Rourke <erinorourke17@gmail.com>
2022-03-18 11:34:22 -0500

    integrated to read and write to database

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 import datetime
-
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from google.cloud import datastore
+import requests
 
+
+API_KEY = os.getenv('MAPS_TOKEN')
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+project_id = 'diomo-servicetimes'
 app = Flask(__name__)
 
-datastore_client = datastore.Client()
+def create_client():
+    from google.cloud import storage
 
-def store_time(dt):
-    entity = datastore.Entity(key=datastore_client.key('visit'))
-    entity.update({
-        'timestamp': dt
-    })
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client.from_service_account_json(
+        GOOGLE_APPLICATION_CREDENTIALS)
+    return storage_client
 
-    datastore_client.put(entity)
+def add_church(client: datastore.Client):
+    kind = "church"
+    # The name/ID for the new entity
+    name = "St. Paul's"
+    # The Cloud Datastore key for the new entity
+    task_key = client.key(kind, name)
 
+    # Prepares the new entity
+    task = datastore.Entity(key=task_key)
+    task["address"] = "6518 Michigan Ave, St. Louis, MO 63111"
 
-def fetch_times(limit):
-    query = datastore_client.query(kind='visit')
-    query.order = ['-timestamp']
+    # Saves the entity
+    client.put(task)
 
-    times = query.fetch(limit=limit)
-
-    return times
-
+def list_churches(client: datastore.Client):
+    query = client.query(kind="church")
+    return(list(query.fetch()))
 
 @app.route('/')
 def root():
-    # Store the current access time in Datastore.
-    store_time(datetime.datetime.now(tz=datetime.timezone.utc))
 
-    # Fetch the most recent 10 access times from Datastore.
-    times = fetch_times(10)
-
+    x = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key='+API_KEY)
+    #from database get list of churches and print as list
+    client = create_client()
+    ds_client = datastore.Client()
+    add_church(ds_client)
+    churches = list_churches(ds_client)
     return render_template(
-        'index.html', times=times)
+        'index.html', churches=churches, response=x.content)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
