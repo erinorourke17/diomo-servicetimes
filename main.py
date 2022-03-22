99b411bdeadb67519f965476aca63fc58e236ce0
Erin O'Rourke <erinorourke17@gmail.com>
2022-03-22 17:44:05 -0500

    no longer breaks with long distances, invalid input; set up landing page

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#TODO: clean up methods/variable names
+#TODO determine what to output in default case (no address given yet)
+#TODO add miles to the end of the distances
+#TODO add service times 2
+
+
 import datetime
 from flask import Flask, request, render_template
 import os
@@ -7,6 +13,7 @@ from google.cloud import datastore
 import requests
 import googlemaps
 import re
+import sys
 
 
 API_KEY = os.getenv('MAPS_TOKEN')
@@ -26,6 +33,20 @@ class Church:
         return self.name + " " + self.address
     def set_dist(self, distance):
         self.distance = distance
+
+class UserLoc:
+    def __init__ (self, user_input):
+        self.input = user_input
+        self.geocode = None
+        self.valid = True
+    def set_geocode(self, geocode):
+        self.geocode = geocode
+
+    def get_geocode(self):
+        return self.geocode
+
+    def set_valid(self):
+        self.valid = True
 
 #def create_client():
 #    from google.cloud import storage
@@ -59,19 +80,22 @@ def list_churches(client: datastore.Client):
 
 def get_geocode(user_input):
     #convert address from user to geopoint
-    #location_formatted = user_input.replace(" ", "+")
-    #x = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+location_formatted+',+CA&key='+API_KEY)
-    georesponse = gmaps.geocode(user_input)
-    #georesponse = x.json()
-    if (len(georesponse) ==  0):
-        return "invalid location"
-    else:
-        return (georesponse[0]['geometry']['location'])
+    georesponse = gmaps.geocode(user_input.input)
+    print(georesponse, file=sys.stderr)
+    try:
+        geocode = georesponse[0]['geometry']['location']
+        user_input.set_geocode(geocode)
+    except IndexError:
+        user_input.valid = False
+    return user_input
 
-def calculate_user_locs (user_loc, churches):
+
+def calculate_distances(user_loc, churches):
     for church in churches:
-        dist =(gmaps.directions(user_loc, church.address)[0]['legs'][0]['distance']['text'])
-        intdist = float(re.findall("\d+\.\d+", dist)[0])
+        dist =(gmaps.directions(user_loc.geocode, church.address)[0]['legs'][0]['distance']['text'])
+        #print(dist, file=sys.stderr)
+        intdist = float(re.findall("\d*\.\d+|\d*", dist)[0]) # match either round # of miles or decimal
+
         church.set_dist(intdist)
 
 def sort_churches (churches):
@@ -81,28 +105,28 @@ def sort_churches (churches):
 
 @app.route('/')
 def root():    
-    #from database get list of churches
-    #determine distance between churches in db and user address
-    #print in order
-
     #client = create_client()
     ds_client = datastore.Client()
     #add_church(ds_client)
     churches = list_churches(ds_client)
-    return render_template(
-        'index.html', churches=churches, response='none')
+    return render_template('home.html')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    user_input = request.form['location']
-    
+    #from database get list of churches
+    #determine distance between churches in db and user address
+    #print in order
     ds_client = datastore.Client()
-    #add_church(ds_client)
     churches = list_churches(ds_client)
-    geocode = get_geocode(user_input)
-    calculate_user_locs(geocode, churches)
-    x = sort_churches(churches)
-    return render_template('index.html', churches=x, response = "it's working!!")
+    user_input = UserLoc(request.form['location'])
+    user_input = get_geocode(user_input)
+    if (user_input.valid): #if the geocode is valid
+        calculate_distances(user_input, churches) #get distance between user-input location and churches in database
+        x = sort_churches(churches) #sort churches by distance from user
+        return render_template('index.html', churches=x, response = "it's working!!")
+    else:
+        return render_template('index.html', response = "invalid location given")
+    
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
